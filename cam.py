f3abbed9f2f839976e3c1925f21011e52ffda020
Brian Vu <brian.vu18@gmail.com>
2025-12-30 13:56:45 -0800

    convert a frame to png to base64

--- cam.py
+++ cam.py
@@ -1,11 +1,29 @@
 import cv2
+import base64
+
 cam = cv2.VideoCapture(0)
 
 if not cam.isOpened():
   print("Camera cannot be opened")
   exit()
 
-while True:
+def convert(frame):
+  name = 'output.png'
+  output = cv2.imwrite(name, frame)
+
+  if not output:
+    print("Could not save image!")
+  else:
+    with open("output.png", "rb") as img:
+      s = base64.b64encode(img.read())
+    with open('encode.bin', "wb") as f:
+      f.write(s)
+
+    print(s)
+    
+fred = True
+while fred:
+  fred = False
   ret, frame = cam.read()
 
   if not ret:
@@ -14,6 +32,8 @@ while True:
 
   frame = cv2.flip(frame, 1)
   cv2.imshow("Camera Feed", frame)
+
+  convert(frame)
 
   if cv2.waitKey(5) == 27:
     break
